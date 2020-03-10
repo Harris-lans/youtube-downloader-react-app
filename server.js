8294553261bc40ba4edf3af4be774d49bef10878
Harish Kumar <harishsghp@gmail.com>
2020-03-10 20:25:37 +0530

    Add basic components for app

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express');
-const cors = require('cors'); 
+const cors = require('cors');
 const ytdl = require('ytdl-core');
 const app = express();
 
 // Utility imports
 const path = require('path');
-const bodyParser = require('body-parser');
 
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(cors());
@@ -13,11 +12,12 @@ app.use(cors());
 app.listen(process.env.PORT || 8080, () => {
 
   console.log(`Server started at port ${process.env.PORT || 8080}`);
+
 });
 
 // processing download request
 app.get('/download', function (req, res) {
-  
+
   var videoURL = req.query.video_url;
 
   console.log(videoURL);
@@ -27,4 +27,5 @@ app.get('/download', function (req, res) {
 
   // using ytdl to pipe the download to the client
   ytdl(videoURL, { format : 'mp4' }).pipe(res);
+
 });
